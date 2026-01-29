af31facf50943ecf1518bd59c50e25121aa82ec5
Kunal Kharat <kunalkharat774@gmail.com>
2026-01-29 15:49:05 +0530

    Add files via upload
     This site is illegal don't its misuse. This is for Educational Purpose

---
+++ phish.py
@@ -0,0 +1,17 @@
+
+# Swap value Example
+
+a=int(input("Enter the value of a:"));
+b=int(input("Enter the value of b:"));
+print("Before Swaping")
+print("A",a)
+print("B",b)
+
+# using three  variables
+
+c=a;
+a=b;
+b=c;
+print("After Swaping")
+print("A",a)
+print("B",b)
